d753c95634727764656995509d1543c2c7a414cf
Shayaaa <shahaslucifer@gmail.com>
2022-02-08 10:26:44 -0500

    Committing base version of code

---
+++ batch1ca-extra/src/main/java/com/controller/extraController.java
@@ -0,0 +1,22 @@
+package com.controller;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class extraController {
+	
+	@GetMapping("displaySum")
+	String displayFactPage(){
+		
+		return "Ex";
+		
+	}
+	
+
+
+}
